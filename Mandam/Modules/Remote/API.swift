07501cf67883ef0df47c3bc2736f8f1b66f68b07
Hong-Sen <20181053@sungshin.ac.kr>
2022-02-18 16:45:30 +0900

    [feat] API 구현

--- Mandam/Modules/Remote/API.swift
+++ Mandam/Modules/Remote/API.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Alamofire
+import RealmSwift
+
+class API {
+    // 수정하지 못하게
+    private init() {}
+    
+    // 싱글톤
+    static let shared: API = API()
+    
+    let base_url = "https://unsplash.com/"
+    
+    func getHeaders(withAuthorization: Bool = false) -> HTTPHeaders? {
+        var headers: HTTPHeaders = [
+            "content-language": Locale.preferredLanguages.first ?? "ko-KR",
+            "platform": "ios",
+            "Authorization": "5Wht8VjoBAg4yaRLd5P26SF_S0ze7VZFi_HPk6CNUSc"
+        ]
+        if withAuthorization {
+            let realm = try! Realm()
+            if let jwtToken = realm.objects(User.self).first?.jwtToken {
+                headers.add(HTTPHeader(name: "Authorization", value: jwtToken))
+            }
+        }
+        return headers
+    }
+    
+}
