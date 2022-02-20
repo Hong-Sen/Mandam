975186babd6f07c86e2701bd7533eb5662a09506
Hong-Sen <20181053@sungshin.ac.kr>
2022-02-20 22:22:50 +0900

    서버로 부터 받을 유저 정보 구현

--- Mandam/Model/Remote/ProfileResponse.swift
+++ Mandam/Model/Remote/ProfileResponse.swift
@@ -1,8 +1,35 @@
 //
 //  ProfileResponse.swift
-//  Mandam
 //
 //  Created by 홍세은 on 2022/02/15.
 //
 
 import Foundation
+
+struct ProfileResponse: Codable {
+    
+    var data: Data?
+    
+    struct Data: Codable {
+        
+        var type: LoginType
+        
+        var email: String
+        var userId: String
+        var userName: String
+        var profileImage: String
+        
+    }
+    
+    // ????
+    class ProfileResponseDecoder: JSONDecoder {
+        let dateFormatter = DateFormatter()
+
+        override init() {
+            super.init()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+            dateDecodingStrategy = .formatted(dateFormatter)
+        }
+    }
+}
